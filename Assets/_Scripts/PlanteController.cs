73c360eaac06849e093d7c9ce074968b2a268c39
exopole <alexandre.nicaise28@gmail.com>
2018-02-21 11:03:36 +0100

    Add outlineEffect and some controller : player, plante

---
+++ Assets/_Scripts/PlanteController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanteController : MonoBehaviour {
+
+    public PlanteController instance;
+
+    public Dictionary<string, GameObject> planteDict;
+}
