6d0333d8db51f8a893c470bd7c9acad2a6e1a09b
exopole <alexandre.nicaise28@gmail.com>
2018-02-26 09:46:39 +0100

    ajout des machines de graines

---
+++ Assets/rotateConstantly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateConstantly : MonoBehaviour {
+
+    public float rotateSpeed; //set it in the  inspector
+
+    void Update()
+    {
+        rotate();
+    }
+
+
+    void rotate()
+    {
+        transform.Rotate(new Vector3(0, 0, rotateSpeed));
+    }
+
+}
