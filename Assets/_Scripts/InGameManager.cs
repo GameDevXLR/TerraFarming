8097ed337b98833df181a341da6f1bb0bcf07380
Jeronimoh-PC\Jeronimoh <jeronimoh974.88@gmail.com>
2018-02-28 17:04:19 +0400

    Machine à graine / Brouiilard Particle + wind zone

--- Assets/_Scripts/InGameManager.cs
+++ Assets/_Scripts/InGameManager.cs
@@ -19,6 +19,7 @@ public class InGameManager : MonoBehaviour {
 	public ParticleSystem miningHitParticle;
 
 	public Animator machineAnimator;
+	public Animator machineBushController;
 
 	public GameObject machineCanvas;
 	public GameObject miningCanvas;
