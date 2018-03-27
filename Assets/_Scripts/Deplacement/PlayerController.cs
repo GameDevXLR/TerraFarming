0fb02e2216c50ae5bba87de0eb19ba60ba3e4217
exopole <alexandre.nicaise28@gmail.com>
2018-03-27 09:21:28 +0200

    Création du playercontroller

---
+++ Assets/_Scripts/Deplacement/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpSpeed = 8f;
+    public float gravity = 20f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    CharacterController Cc;
+
+
+
+    private void Start()
+    {
+        Cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+
+        if (Cc.isGrounded)
+    {       moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //transform.TransformVector(moveDirection);
+            if (moveDirection != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), 0.15F);
+            moveDirection.Normalize() ;
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else if (!Cc.isGrounded)
+        {
+
+            transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 10);
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+
+        Cc.Move(moveDirection * Time.deltaTime);
+    }
+
+}
