c5326f8b59b6675e55e1f7da2550554e6441f2e3
exopole <alexandre.nicaise28@gmail.com>
2018-05-07 20:36:45 +0200

    meilleurs transition et moins de bug
    A voir pour les transitions

---
+++ Assets/_Scripts/AnimatorState/Action/VictoryPlayerState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryPlayerState : StateMachineBehaviour {
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        if(CustomInputManager.instance.getDirection() != Vector3.zero)
+        {
+            InGameManager.instance.playerController.anim.SetBool("iswalking", true);
+        }
+    }
+}
