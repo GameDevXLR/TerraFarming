8fe2df12956a0a4551640f84d46755d3c698798c
Jeronimoh <Jeronimoh974.88@gmail.com>
2018-06-11 09:10:39 +0400

    Pret pour recuperation IGmanager

--- Assets/TimelineManager.cs
+++ Assets/TimelineManager.cs
@@ -9,12 +9,23 @@ public class TimelineManager : MonoBehaviour
 	public static TimelineManager instance;
 
 	public PlayableDirector director;
+	public PlayableAsset cutscene1;
+
+	public Animator captainAnimator;
 
 	void Awake()
 	{
 		if (instance == null) 
 		{
 			instance = this;
+		}
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+//			captainAnimator.SetBool ("iswalking", true);
+			director.Play (cutscene1);
 		}
 	}
 }
