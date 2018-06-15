3630fadd7be399b27c23883be61f850369f73e02
Marshal974 <mez.guillaume@gmail.com>
2018-06-15 11:24:31 +0400

    nouvelle collection

---
+++ Assets/PlayerUICanvas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUICanvas : MonoBehaviour 
+{
+	[Header("Les variables lié a la collection:")]
+	public Transform collectionPanel;
+	public Image showNotOwned;
+	public Image showCave;
+	public Image showPlain;
+	public Image showCrater;
+
+	public Color visibleBtnColor;
+	public Color hidenBtnColor;
+
+	public void ChangeCaveUIColor()
+	{
+		if (showCave.color == hidenBtnColor) 
+		{
+			showCave.color = visibleBtnColor;
+		} else {
+			showCave.color = hidenBtnColor;
+
+		}
+	}
+	public void ChangePlainUIColor()
+	{
+		if (showPlain.color == hidenBtnColor) 
+		{
+			showPlain.color = visibleBtnColor;
+		} else {
+			showPlain.color = hidenBtnColor;
+
+		}
+	}
+	public void ChangeCraterUIColor()
+	{
+		if (showCrater.color == hidenBtnColor) 
+		{
+			showCrater.color = visibleBtnColor;
+		} else {
+			showCrater.color = hidenBtnColor;
+
+		}
+	}
+	public void ChangeNotOwnedUIColor()
+	{
+		if (showNotOwned.color == hidenBtnColor) 
+		{
+			showNotOwned.color = visibleBtnColor;
+		} else {
+			showNotOwned.color = hidenBtnColor;
+
+		}
+	}
+}
