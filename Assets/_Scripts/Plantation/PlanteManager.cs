ae8b541b4b9369b4ac6c187d38c1767434483e41
Marshal974 <mez.guillaume@gmail.com>
2018-06-29 17:57:13 +0400

    debugging en cours

--- Assets/_Scripts/Plantation/PlanteManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class PlanteManager : MonoBehaviour
-{
-    public PlanteManager instance;
-
-    public Dictionary<string, GameObject> planteDict;
-}
--- Assets/_Scripts/Utils/UtilsAnimation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class UtilsAnimation
-{
-    public static void SwitchAnime(Animator anim, AnimeParameters anime, bool activate)
-    {
-        anim.SetBool(anime.ToString(), activate);
-    }
-}
